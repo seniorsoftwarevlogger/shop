e56a2e09914627c2862b6c9c7bd334d72d6c7e17
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2024-10-03 14:18:59 +0200

    feat: About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 function TermsPage() {
   return (
     <div className="max-w-xl mx-auto prose prose-blue">
@@ -5,19 +7,24 @@ function TermsPage() {
       <p>
         You&apos;re viewing the{" "}
         <a
-          href="https://headlessdropshipping.com/"
-          title="Learn more about Headless Dropshipping"
+          href="https://shop.seniorsoftwarevlogger.com/"
+          title="Learn more about Senior Software Vlogger"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Headless Dropshipping Starter
+          Senior Software Vlogger Shop
         </a>{" "}
-        by Jamie Barton.
+        by Dima Rozhkov.
       </p>
       <p>
-        Build your own Headless eCommerce storefront with Next.js and Snipcart,
-        and deliver swag powered by Printful. End to end commerce, styled using
-        TailwindCSS and deployed on Vercel.
+        Dima is a software developer, manager, content creator, and a dad. He is passionate
+        about software, vlogging, and spending time with his family.
+      </p>
+      <p>
+        You can find more about Dima&apos;s work on his website <a href="https://seniorsoftwarevlogger.com" target="_blank" rel="noopener noreferrer">Senior Software Vlogger</a>.
+      </p>
+      <p>
+        <Image src="/images/dima.jpg" alt="Dima Rozhkov" width={400} height={400} />
       </p>
     </div>
   );
