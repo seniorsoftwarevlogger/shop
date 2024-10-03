1fb22e68f193616dace156dad77b4405b39d7460
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2024-10-03 14:18:42 +0200

    chore: Update terms of sale page with correct contact email addresses

--- src/pages/terms-of-sale.tsx
+++ src/pages/terms-of-sale.tsx
@@ -3,16 +3,16 @@ function TermsPage() {
     <div className="max-w-2xl mx-auto prose prose-blue">
       <h2>What&apos;s your return policy?</h2>
       <p>
-        We don&apos;t offer returns and exchanges, but if there&apos;s something
-        wrong with your order, please let us know by contacting us at [insert
-        your support email here]!
+        The products are being produced on demand, so we don&apos;t offer returns and exchanges, but if there&apos;s something
+        wrong with your order, please let us know by contacting us at{" "}
+        <a href="mailto:dima@seniorsoftwarevlogger.com">shop@seniorsoftwarevlogger.com</a>!
       </p>
 
       <h2>Do you offer refunds?</h2>
       <p>
         Refunds are only offered to customers that receive the wrong items or
-        damaged items. If any of these apply, please contact us at [insert your
-        support email here] with photos of wrong/damaged items and we&apos;ll
+        damaged items. If any of these apply, please contact us at{" "}
+        <a href="mailto:dima@seniorsoftwarevlogger.com">shop@seniorsoftwarevlogger.com</a> with photos of wrong/damaged items and we&apos;ll
         sort that out for you.
       </p>
 
@@ -22,8 +22,8 @@ function TermsPage() {
         size would fit better, check out our sizing charts—we have one for every
         item listed on our store, in the product description section. Though
         rare, it&apos;s possible that an item you ordered was mislabelled. If
-        that&apos;s the case, please let us know at [insert your support email
-        here] within a week after receiving your order. Include your order
+        that&apos;s the case, please let us know at{" "}
+        <a href="mailto:dima@seniorsoftwarevlogger.com">shop@seniorsoftwarevlogger.com</a> within a week after receiving your order. Include your order
         number and photos of the mislabeled item, and we&apos;ll send you a new
         one, or issue a refund!
       </p>
