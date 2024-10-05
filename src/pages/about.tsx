2ad4106e9fe61a95f460e3c4624a8cf161de23b2
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2024-10-05 16:39:55 +0200

    chore: Update About page content and image size

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,26 +5,16 @@ function TermsPage() {
     <div className="max-w-xl mx-auto prose prose-blue">
       <h2>Welcome</h2>
       <p>
-        You&apos;re viewing the{" "}
-        <a
-          href="https://shop.seniorsoftwarevlogger.com/"
-          title="Learn more about Senior Software Vlogger"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Senior Software Vlogger Shop
-        </a>{" "}
-        by Dima Rozhkov.
+        Hey, welcome to my shop! I&apos;m Dima, a software developer, manager, content creator, and a dad.
       </p>
       <p>
-        Dima is a software developer, manager, content creator, and a dad. He is passionate
-        about software, vlogging, and spending time with his family.
+        I&apos;ve been creating software for over 17 years and I&apos;ve been vlogging about software development for over 10 years. Here you can find some of my favorite memes around software development that I&apos;ve found and created.
       </p>
       <p>
-        You can find more about Dima&apos;s work on his website <a href="https://seniorsoftwarevlogger.com" target="_blank" rel="noopener noreferrer">Senior Software Vlogger</a>.
+        You can find more about my work on my website <a href="https://seniorsoftwarevlogger.com" target="_blank" rel="noopener noreferrer">Senior Software Vlogger</a>.
       </p>
-      <p>
-        <Image src="/images/dima.jpg" alt="Dima Rozhkov" width={400} height={400} />
+      <p className="mt-8 flex justify-center">
+        <Image src="/images/dima.jpg" alt="Dima Rozhkov" width={200} height={200} className="rounded-full" />
       </p>
     </div>
   );
